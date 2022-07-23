bdc70d65b814b5a6708857b7c90611cef1442b0c
rogaandrei <109062431+rogaandrei@users.noreply.github.com>
2022-07-23 18:52:39 +0300

    Am adaugat noi exercitii

---
+++ Curs/Exercitii_din_fisierul_text/Curs1/Ex1.java
@@ -0,0 +1,8 @@
+package Exercitii_din_fisierul_text.Curs1;
+
+public class Ex1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ Curs/Exercitii_din_fisierul_text/Curs1/Ex2.java
@@ -0,0 +1,14 @@
+package Exercitii_din_fisierul_text.Curs1;
+
+public class Ex2 {
+    static int val1=50;
+    static int val2=40;
+
+    public static void main(String[] args) {
+        if(val1<val2){
+            System.out.println("Valoarea 2 este mai mare ca valoarea 1");
+        }else{
+            System.out.println("Valoarea 1 este mai mare ca valoarea 2");
+        }
+    }
+}
