cc6231443552c373108baba2ecaef8b94fd5cb47
rogaandrei <109062431+rogaandrei@users.noreply.github.com>
2022-07-27 09:41:43 +0300

    Am formatat exercitiile

--- Curs/Exercitii_din_fisierul_text/Curs1/Ex2.java
+++ Curs/Exercitii_din_fisierul_text/Curs1/Ex2.java
@@ -1,13 +1,13 @@
 package Exercitii_din_fisierul_text.Curs1;
 
 public class Ex2 {
-    static int val1=50;
-    static int val2=40;
+    static int val1 = 50;
+    static int val2 = 40;
 
     public static void main(String[] args) {
-        if(val1<val2){
+        if (val1 < val2) {
             System.out.println("Valoarea 2 este mai mare ca valoarea 1");
-        }else{
+        } else {
             System.out.println("Valoarea 1 este mai mare ca valoarea 2");
         }
     }
--- Curs/Exercitii_din_fisierul_text/Curs1/Ex3.java
+++ Curs/Exercitii_din_fisierul_text/Curs1/Ex3.java
@@ -3,24 +3,25 @@ package Exercitii_din_fisierul_text.Curs1;
 import java.util.Scanner;
 
 public class Ex3 {
-   static int n;
-   static int sum=0;
+    static int n;
+    static int sum = 0;
 
     public static void main(String[] args) {
         System.out.println("Introduceti un numar:");
-        Scanner scanner=new Scanner(System.in);
-        n=scanner.nextInt();
-    if(n%2!=0){
-       for(int i=1;i<n;i=i+2){
-           System.out.println("numarul afisat este:"+i);        }
-   }
-    if(n%2==0){
-        for(int i=0;i<n;i=i+2){
-             sum = sum + i;
-            System.out.println("Suma nr pare este egala cu " +sum);
+        Scanner scanner = new Scanner(System.in);
+        n = scanner.nextInt();
+        if (n % 2 != 0) {
+            for (int i = 1; i < n; i = i + 2) {
+                System.out.println("numarul afisat este:" + i);
+            }
         }
+        if (n % 2 == 0) {
+            for (int i = 0; i < n; i = i + 2) {
+                sum = sum + i;
+                System.out.println("Suma nr pare este egala cu " + sum);
+            }
 
-    }
+        }
     }
 }
 
